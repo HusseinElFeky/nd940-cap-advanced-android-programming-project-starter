6ed0bb59671e9de72e938dbf7c9ca27468bc145a
Hussein El Feky <hussein.feky@gmail.com>
2020-05-17 17:26:04 +0200

    Refactor code structure

--- starter/app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDatabase.kt
+++ starter/app/src/main/java/com/example/android/politicalpreparedness/database/ElectionDatabase.kt
@@ -5,11 +5,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
-import com.example.android.politicalpreparedness.network.models.Election
+import com.example.android.politicalpreparedness.models.Election
 
 @Database(entities = [Election::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
-abstract class ElectionDatabase: RoomDatabase() {
+abstract class ElectionDatabase : RoomDatabase() {
 
     abstract val electionDao: ElectionDao
 
@@ -23,12 +23,12 @@ abstract class ElectionDatabase: RoomDatabase() {
                 var instance = INSTANCE
                 if (instance == null) {
                     instance = Room.databaseBuilder(
-                            context.applicationContext,
-                            ElectionDatabase::class.java,
-                            "election_database"
+                        context.applicationContext,
+                        ElectionDatabase::class.java,
+                        "election_database"
                     )
-                            .fallbackToDestructiveMigration()
-                            .build()
+                        .fallbackToDestructiveMigration()
+                        .build()
 
                     INSTANCE = instance
                 }
